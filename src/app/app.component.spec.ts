f10d0fbeb2530ae8be916bcf377ee587a0171d8e
Kaviya Sri Rajkumar <kaviyasri14@gmail.com>
2018-07-23 09:49:53 +0530

    unit testing for primeng components

---
+++ src/app/app.component.spec.ts
@@ -0,0 +1,41 @@
+import { TestBed, async } from '@angular/core/testing';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { FormsModule } from '@angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+import{HttpModule}from '@angular/http';
+import { ButtonModule } from 'primeng/button';
+
+describe('AppComponent', () => {
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent,
+        LoginComponent
+      ],
+      imports: [
+        FormsModule,
+        ButtonModule,
+        ReactiveFormsModule,        
+           HttpModule ,
+          
+      ]
+    }).compileComponents();
+  }));
+  it('should create the app', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app).toBeTruthy();
+  }));
+  it(`should have as title 'app'`, async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.debugElement.componentInstance;
+    expect(app.title).toEqual('app');
+  }));
+  it('should render title in a h1 tag', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    const compiled = fixture.debugElement.nativeElement;
+    expect(compiled.querySelector('h1').textContent).toContain('PrimeNg');
+  }));
+});
